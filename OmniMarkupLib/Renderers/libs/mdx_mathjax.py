209297cbedceec5f2fc27934b16edf51b7e99e22
Timon Wong <timon86.wang@gmail.com>
2012-11-02 23:54:41 +0800

    Fix mathjax for markdown (handles delimiters)

--- OmniMarkupLib/Renderers/libs/mdx_mathjax.py
+++ OmniMarkupLib/Renderers/libs/mdx_mathjax.py
@@ -1,7 +1,7 @@
 import markdown
 
 
-class MathJaxPattern1(markdown.inlinepatterns.Pattern):
+class MathJaxPattern(markdown.inlinepatterns.Pattern):
     def __init__(self):
         markdown.inlinepatterns.Pattern.__init__(self, r'(?<!\\)(\$\$?)(.+?)\2')
 
@@ -11,9 +11,19 @@ class MathJaxPattern1(markdown.inlinepatterns.Pattern):
         return node
 
 
-class MathJaxPattern2(markdown.inlinepatterns.Pattern):
+class MathJaxNativeInlinePattern(markdown.inlinepatterns.Pattern):
     def __init__(self):
-        markdown.inlinepatterns.Pattern.__init__(self, r'(?<!\\)(\\\()(.+?)(\\\))')
+        markdown.inlinepatterns.Pattern.__init__(self, r'(\\\()(.+?)(\\\))')
+
+    def handleMatch(self, m):
+        node = markdown.util.etree.Element('mathjax')
+        node.text = markdown.util.AtomicString(m.group(2) + m.group(3) + m.group(4))
+        return node
+
+
+class MathJaxNativeDisplayPattern(markdown.inlinepatterns.Pattern):
+    def __init__(self):
+        markdown.inlinepatterns.Pattern.__init__(self, r'(\\\[)(.+?)(\\\])')
 
     def handleMatch(self, m):
         node = markdown.util.etree.Element('mathjax')
@@ -24,8 +34,9 @@ class MathJaxPattern2(markdown.inlinepatterns.Pattern):
 class MathJaxExtension(markdown.Extension):
     def extendMarkdown(self, md, md_globals):
         # Needs to come before escape matching because \ is pretty important in LaTeX
-        md.inlinePatterns.add('mathjax', MathJaxPattern1(), '<escape')
-        md.inlinePatterns.add('mathjax', MathJaxPattern2(), '<escape')
+        md.inlinePatterns.add('mathjax1', MathJaxPattern(), '<escape')
+        md.inlinePatterns.add('mathjax2', MathJaxNativeInlinePattern(), '<escape')
+        md.inlinePatterns.add('mathjax3', MathJaxNativeDisplayPattern(), '<escape')
 
 
 def makeExtension(configs=None):
--- templates/github.tpl
+++ templates/github.tpl
@@ -32,6 +32,7 @@
     MathJax.Hub.Config({
         tex2jax: {
           inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+          displayMath: [ ['$$', '$$'], ["\\[", "\\]"] ],
           processEscapes: true
         },
         TeX: {
