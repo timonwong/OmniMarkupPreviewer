6dd53a29cc29bfca43758efbf68c2a17a10422a8
Timon Wong <timon86.wang@gmail.com>
2013-03-14 19:28:10 +0800

    Add python3.x docutils

---
+++ OmniMarkupLib/Renderers/libs/python3/docutils/parsers/null.py
@@ -0,0 +1,20 @@
+# $Id$
+# Author: Martin Blais <blais@furius.ca>
+# Copyright: This module has been placed in the public domain.
+
+"""A do-nothing parser."""
+
+from docutils import parsers
+
+
+class Parser(parsers.Parser):
+
+    """A do-nothing parser."""
+
+    supported = ('null',)
+
+    config_section = 'null parser'
+    config_section_dependencies = ('parsers',)
+
+    def parse(self, inputstring, document):
+        pass
