43fb4626a4e1045f5e49891f52644748f9679ce0
Timon Wong <timon86.wang@gmail.com>
2013-03-21 21:01:35 +0800

    reraise from six.py

--- OmniMarkupLib/Common.py
+++ OmniMarkupLib/Common.py
@@ -33,9 +33,36 @@ if g_is_py3k:
     import html.entities as htmlentitydefs
     import _thread as thread
     unichr = chr
+
+    # from six.py (by Benjamin Peterson)
+    import builtins
+    exec_ = getattr(builtins, "exec")
+
+    def reraise(tp, value, tb=None):
+        if value.__traceback__ is not tb:
+            raise value.with_traceback(tb)
+        raise value
+
 else:
     import htmlentitydefs
     import thread
+
+    # from six.py (by Benjamin Peterson)
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
+
+    exec_("""def reraise(tp, value, tb=None):
+    raise tp, value, tb
+""")
 
 
 def entities_unescape(text):
@@ -322,9 +349,7 @@ class Future(object):
             while not self.__done:
                 self.__cond.wait()
         if self.__except:
-            if g_is_py3k:
-                exec('raise self.__except[0](self.__except[1]).with_traceback(self.__except[2])')
-            else:
-                exec('raise self.__except[0], self.__except[1], self.__except[2]')
+            exc = self.__except
+            reraise(exc[0], exc[1], exc[2])
         result = copy.deepcopy(self.__result)
         return result
